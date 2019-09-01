f58298e83ef46d850864ef8a31e1845b27c3ebb1
Shaik Aqib <shaik@L-156179465.broadband>
2019-09-01 18:26:50 +0100

    second commit

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ import update from 'immutability-helper'
 
 import * as lib from './lib'
 import RNFS from 'react-native-fs';
-const { SampleTable, Header, Colors, PainTable,FlexionContraction } = lib
+const { SampleTable, Header, Colors, PainTable, FlexionContraction,ExtensionLag,Anteroposterior,Mediolateral,FlexionRange,Alignment } = lib
 
 
 export default class App extends Component {
@@ -41,12 +41,94 @@ export default class App extends Component {
       score: 0,
       csvArr: []
     },
-    FlexionContractionTable : {
+    FlexionContractionTable: {
+      tableData: {
+        "5°-10°": -5,
+        "10°-15°": -10,
+        "15°-20°": -15,
+        ">20°": -15
+      },
+      clickedIndex: null,
+      score: 0,
+      csvArr: []
+    },
+    ExtensionLagTable : {
       tableData:{
-        "5°-10°":-5,
-        "10°-15°":-10,
-        "15°-20°":-15,
+        "<10°":-5,
+        "10°-20°":-10,
         ">20°":-15
+      },
+      clickedIndex:null,
+      score:0,
+      csvArr:[]
+    },
+    AnteroposteriorTable:{
+      tableData:{
+        "<5mm":5,
+        "5-10mm":0,
+        "10+mm":0
+      },
+      clickedIndex:null,
+      score:0,
+      csvArr:[]
+    },
+    MediolateralTable:{
+      tableData:{
+        "<5°":15,
+        "6-9°":10,
+        "10-14°":5,
+        "15°":0
+      },
+      clickedIndex : null,
+      score:0,
+      csvArr : []
+    },
+    FlexionRangeTable:{
+      tableData:{
+        "0-5":1,
+        "6-10":1,
+        "11-15":1,
+        "16-20":1,
+        "21-25":1,
+        "26-30":2,
+        "31-35":2,
+        "36-40":2,
+        "41-45":2,
+        "46-50":2,
+        "51-55":3,
+        "56-60":3,
+        "61-65":3,
+        "66-70":3,
+        "71-75":3,
+        "76-80":4,
+        "81-85":4,
+        "86-90":4,
+        "91-95":4,
+        "96-100":4,
+        "101-105":5,
+        "106-110":5,
+        "111-115":5,
+        "116-120":5,
+        "121-125":5
+      },
+      clickedIndex:null,
+      score:0,
+      csvArr:[]
+    },
+    AlignmentTable : {
+      tableData:{
+        "0":-10,
+        "1":-10,
+        "2":25,
+        "3":25,
+        "4":25,
+        "5-10":25,
+        "11":-10,
+        "12":-10,
+        "13":-10,
+        "14":-10,
+        "15":-10,
+        "Over 15°":-10
       },
       clickedIndex:null,
       score:0,
@@ -68,14 +150,118 @@ export default class App extends Component {
       csvArr: { $set: ['Pain', Object.keys(newPainTable.tableData).find((key, i) => i === index), Object.values(newPainTable.tableData).find((value, i) => i === index)] }
     });
     let newTotalScore = totalScore + newPainTable.score
-    this.setState({ PainTable: newPainTable })
+    this.setState({ PainTable: newPainTable, totalScore: newTotalScore })
+  }
+
+  onFlexionContractionSelect = (index, e) => {
+    e.preventDefault();
+    const { FlexionContractionTable, totalScore } = this.state;
+    let newFlexionContractionTable = FlexionContractionTable;
+
+    newFlexionContractionTable = update(newFlexionContractionTable, {
+      clickedIndex: { $set: index },
+      score: { $set: Object.values(newFlexionContractionTable.tableData).find((value, i) => i === index) },
+      csvArr: { $set: ['Flexion Contracture (if present)', Object.keys(newFlexionContractionTable.tableData).find((key, i) => i === index), Object.values(newFlexionContractionTable.tableData).find((value, i) => i === index)] }
+    });
+    let newTotalScore = totalScore + newFlexionContractionTable.score;
+    this.setState({ FlexionContractionTable: newFlexionContractionTable, totalScore: newTotalScore });
+  }
+
+  onExtensionLagSelect = (index,e) => {
+    e.preventDefault();
+    const {ExtensionLagTable,totalScore} = this.state;
+    let newExtensionLagTable = ExtensionLagTable;
+
+    newExtensionLagTable = update(newExtensionLagTable,{
+      clickedIndex: { $set: index },
+      score: { $set: Object.values(newExtensionLagTable.tableData).find((value, i) => i === index) },
+      csvArr: { $set: ['Extension lag', Object.keys(newExtensionLagTable.tableData).find((key, i) => i === index), Object.values(newExtensionLagTable.tableData).find((value, i) => i === index)] }
+    });
+
+    let newTotalScore = totalScore + newExtensionLagTable.score;
+    this.setState({ ExtensionLagTable: newExtensionLagTable, totalScore: newTotalScore });
+  }
+
+  onAnteroposteriorSelect = (index,e) => {
+    e.preventDefault();
+    const {AnteroposteriorTable,totalScore} = this.state;
+    let newAnteroposteriorTable = AnteroposteriorTable;
+
+    newAnteroposteriorTable = update(newAnteroposteriorTable,{
+      clickedIndex: { $set: index },
+      score: { $set: Object.values(newAnteroposteriorTable.tableData).find((value, i) => i === index) },
+      csvArr: { $set: ['ntero-posterior', Object.keys(newAnteroposteriorTable.tableData).find((key, i) => i === index), Object.values(newAnteroposteriorTable.tableData).find((value, i) => i === index)] }
+    });
+
+    let newTotalScore = totalScore + newAnteroposteriorTable.score;
+    this.setState({ AnteroposteriorTable: newAnteroposteriorTable, totalScore: newTotalScore });
+  }
+
+  onMediolateralTableSelect = (index,e) => {
+    e.preventDefault();
+    const {MediolateralTable,totalScore} = this.state;
+    let newMediolateralTable = MediolateralTable;
+
+    newMediolateralTable = update(newMediolateralTable,{
+      clickedIndex: { $set: index },
+      score: { $set: Object.values(newMediolateralTable.tableData).find((value, i) => i === index) },
+      csvArr: { $set: ['Mediolateral', Object.keys(newMediolateralTable.tableData).find((key, i) => i === index), Object.values(newMediolateralTable.tableData).find((value, i) => i === index)] }
+    });
+
+    let newTotalScore = totalScore + newMediolateralTable.score;
+    this.setState({ MediolateralTable: newMediolateralTable, totalScore: newTotalScore });
+  }
+
+  onFlexionRangeSelect = (index,e) => {
+    e.preventDefault();
+    const {FlexionRangeTable,totalScore} = this.state;
+    let newFlexionRangeTable = FlexionRangeTable;
+
+    newFlexionRangeTable = update(newFlexionRangeTable,{
+      clickedIndex: { $set: index },
+      score: { $set: Object.values(newFlexionRangeTable.tableData).find((value, i) => i === index) },
+      csvArr: { $set: ['Total Range of Flexion', Object.keys(newFlexionRangeTable.tableData).find((key, i) => i === index), Object.values(newFlexionRangeTable.tableData).find((value, i) => i === index)] }
+    });
+
+    let newTotalScore = totalScore + newFlexionRangeTable.score;
+    this.setState({ FlexionRangeTable: newFlexionRangeTable, totalScore: newTotalScore });
+  }
+
+  onAlignmentSelect = (index,e) => {
+    e.preventDefault();
+    const {AlignmentTable,totalScore} = this.state;
+    let newAlignmentTable = AlignmentTable;
+
+    newAlignmentTable = update(newAlignmentTable,{
+      clickedIndex: { $set: index },
+      score: { $set: Object.values(newAlignmentTable.tableData).find((value, i) => i === index) },
+      csvArr: { $set: ['Alignment (Varus &amp; Valgus)', Object.keys(newAlignmentTable.tableData).find((key, i) => i === index), Object.values(newAlignmentTable.tableData).find((value, i) => i === index)] }
+    });
+
+    let newTotalScore = totalScore + newAlignmentTable.score;
+    this.setState({ AlignmentTable: newAlignmentTable, totalScore: newTotalScore });
+  }
+
+  isElementUndefined = (arr) => {
+    arr.some((each)=>{
+      return each !== undefined
+    })
   }
 
   saveCsv = () => {
-    const { PainTable } = this.state;
-    const path = RNFS.DocumentDirectoryPath + '/samplePain.csv';
+    const { PainTable,FlexionContractionTable,AlignmentTable,AnteroposteriorTable,ExtensionLagTable,FlexionRangeTable,MediolateralTable,totalScore } = this.state;
+    const path = RNFS.DocumentDirectoryPath + `/${new Date().getTime().toString()}.csv`;
     const values = []
-    values.push(PainTable.csvArr);
+    this.isElementUndefined(PainTable.csvArr) && values.push(PainTable.csvArr);
+    this.isElementUndefined(FlexionContractionTable.csvArr) &&values.push(FlexionContractionTable.csvArr);
+    this.isElementUndefined(AlignmentTable.csvArr) &&values.push(AlignmentTable.csvArr);
+    this.isElementUndefined(AnteroposteriorTable.csvArr) &&values.push(AnteroposteriorTable.csvArr);
+    this.isElementUndefined(ExtensionLagTable.csvArr) &&values.push(ExtensionLagTable.csvArr);
+    this.isElementUndefined(FlexionContractionTable.csvArr) &&values.push(FlexionContractionTable.csvArr);
+    this.isElementUndefined(FlexionRangeTable.csvArr) &&values.push(FlexionRangeTable.csvArr);
+    this.isElementUndefined(MediolateralTable.csvArr) &&values.push(MediolateralTable.csvArr);
+    values.push(['TotalSocre',totalScore])
+
     const headerString = 'Question,Answer,Score\n';
     const rowString = values.map(d => `${d[0]},${d[1]},${d[2]}\n`).join('');
     const csvString = `${headerString}${rowString}`;
@@ -96,7 +282,11 @@ export default class App extends Component {
 
     const PainTableArr = Object.keys(state.PainTable.tableData).map((key) => [key, state.PainTable.tableData[key]]);
     const FlexionContractionArr = Object.keys(state.FlexionContractionTable.tableData).map((key) => [key, state.FlexionContractionTable.tableData[key]])
-
+    const ExtensionLagArr = Object.keys(state.ExtensionLagTable.tableData).map((key) => [key, state.ExtensionLagTable.tableData[key]]);
+    const AnteroposteriorArr = Object.keys(state.AnteroposteriorTable.tableData).map((key) => [key, state.AnteroposteriorTable.tableData[key]]);
+    const MediolateralArr = Object.keys(state.MediolateralTable.tableData).map((key) => [key, state.MediolateralTable.tableData[key]]);
+    const FlexionRangeArr = Object.keys(state.FlexionRangeTable.tableData).map((key) => [key, state.FlexionRangeTable.tableData[key]]);
+    const AlignmentArr = Object.keys(state.AlignmentTable.tableData).map((key) => [key, state.AlignmentTable.tableData[key]]);
     return (
       <Fragment>
         <SafeAreaView>
@@ -106,7 +296,12 @@ export default class App extends Component {
           >
             <Header />
             <PainTable PainTableState={PainTableArr} clickedIndex={state.PainTable.clickedIndex} onSelectMethod={this.onPainTableValueSelect} />
-            <FlexionContraction FlexionContractionState={FlexionContractionArr} clickedIndex={} onSelectMethod={}/>
+            <FlexionContraction FlexionContractionState={FlexionContractionArr} clickedIndex={state.FlexionContractionTable.clickedIndex} onSelectMethod={this.onFlexionContractionSelect} />
+            <ExtensionLag ExtensionLagState={ExtensionLagArr} clickedIndex = {state.ExtensionLagTable.clickedIndex} onSelectMethod = {this.onExtensionLagSelect} />
+            <Anteroposterior AnteroposteriorState={AnteroposteriorArr} clickedIndex = {state.AnteroposteriorTable.clickedIndex} onSelectMethod = {this.onAnteroposteriorSelect} />
+            <Mediolateral MediolateralState={MediolateralArr} clickedIndex ={state.MediolateralTable.clickedIndex} onSelectMethod =  {this.onMediolateralTableSelect} />
+            <FlexionRange FlexionRangeState = {FlexionRangeArr} clickedIndex={state.FlexionRangeTable.clickedIndex} onSelectMethod={this.onFlexionRangeSelect} />
+            <Alignment AlignmentState = {AlignmentArr} clickedIndex = {state.AlignmentTable.clickedIndex} onSelectMethod={this.onAlignmentSelect} />
             <Button
               onPress={this.saveCsv}
               title="Save CSV"
